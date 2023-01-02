75ab73a356cef61ff317837e2e3a70941406b23c
Jione Jang <jenny.jione.jang@gmail.com>
2023-01-02 10:11:48 +0900

    insert_data_front in cache

--- app/cache.py
+++ app/cache.py
@@ -12,6 +12,12 @@ class MarkdictCache:
     def show_cache(self):
         print(self._cache)
         return self._cache
+    
+    def show_cache_2(self):
+        for k in self._cache_key_list:
+            print(self._cache.get(k)['modelResult'])
+        print()
+        print(len(self._cache_key_list))
 
     def insert_data(self, datas):
         check = False
@@ -20,6 +26,18 @@ class MarkdictCache:
             # 캐시에 새로 들어간 값이 있는지 체크
             if oid not in self._cache_key_list:
                 self._cache_key_list.append(oid)
+                self._cache[oid] = data
+                check = True
+        return check
+    
+    def insert_data_front(self, datas):
+        check = False
+        datas.reverse()
+        for data in datas:
+            oid = data["id"]
+            # 캐시에 새로 들어간 값이 있는지 체크
+            if oid not in self._cache_key_list:
+                self._cache_key_list.insert(0, oid)
                 self._cache[oid] = data
                 check = True
         return check
