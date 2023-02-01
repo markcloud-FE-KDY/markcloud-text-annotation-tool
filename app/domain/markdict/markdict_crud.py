47c185ffc7cf81a59d9aa7ef8c67bb5459008761
Jione Jang <jenny.jione.jang@gmail.com>
2023-02-01 13:16:52 +0900

    Modify: remove cache code

--- app/domain/markdict/markdict_crud.py
+++ app/domain/markdict/markdict_crud.py
@@ -7,18 +7,19 @@ from bson.objectid import ObjectId
 import time
 
 
-markdictCache = MarkdictCache()
+# markdictCache = MarkdictCache()
 
 
 # 전체보기
 def retrieve_markdict_list(m: MarkdictList, search_option: list):
     # 캐시 삭제
-    markdictCache.clear_cache()
+    # markdictCache.clear_cache()
 
     markdicts = []
     markdict_list = (
         (mark_dict_collection.find({"$and": search_option}))
         .sort([("dateModified", -1), ("_id", 1)])
+        # .sort([("Check.passCheck", 1), ("dateModified", -1), ("_id", 1)])  # 보류 맨 뒤로
         .skip(m.page * m.size)
         .limit(m.size)
     )
@@ -27,106 +28,173 @@ def retrieve_markdict_list(m: MarkdictList, search_option: list):
     return markdicts
 
 
-def add_cache(m: MarkdictList, search_option: str):
-    markdicts = []
-    markdict_list = (
-    (mark_dict_collection.find({"$and": search_option}))
-    .sort([("dateModified", -1), ("_id", 1)])
-    .skip(m.page * m.size)
-    .limit(m.size * 2)
-    )    
-    for markdict in markdict_list:
-        markdicts.append(markdict_detail_helper(markdict))
-    markdictCache.insert_data(markdicts)
+# def add_cache(m: MarkdictList, search_option: str):
+#     markdicts = []
+#     markdict_list = (
+#     (mark_dict_collection.find({"$and": search_option}))
+#     .sort([("dateModified", -1), ("_id", 1)])
+#     .skip(m.page * m.size)
+#     .limit(m.size * 2)
+#     )    
+#     for markdict in markdict_list:
+#         markdicts.append(markdict_detail_helper(markdict))
+#     markdictCache.insert_data(markdicts)
     
 
-def add_prev_cache(m: MarkdictList, search_option: str):
-    markdicts = []
-    markdict_list = (
-    (mark_dict_collection.find({"$and": search_option}))
-    .sort([("dateModified", -1), ("_id", 1)])
-    .skip(m.page * m.size)
-    .limit(m.size * 2)
-    )    
-    for markdict in markdict_list:
-        markdicts.append(markdict_detail_helper(markdict))
-    markdictCache.insert_data_front(markdicts)
+# def add_prev_cache(m: MarkdictList, search_option: str):
+#     markdicts = []
+#     markdict_list = (
+#     (mark_dict_collection.find({"$and": search_option}))
+#     .sort([("dateModified", -1), ("_id", 1)])
+#     .skip(m.page * m.size)
+#     .limit(m.size * 2)
+#     )    
+#     for markdict in markdict_list:
+#         markdicts.append(markdict_detail_helper(markdict))
+#     markdictCache.insert_data_front(markdicts)
     
     
 # current
-def retrieve_markdict(oid: str, m: MarkdictData) -> dict:
-    cache_hit = markdictCache.find_by_oid(oid)
-    if cache_hit:
-        print(" C A C H E   H I T !")
-        return cache_hit
+# def retrieve_markdict(oid: str, m: MarkdictData) -> dict:
+def retrieve_markdict(oid: str, m: MarkdictList) -> dict:
+    if not oid:
+        markdict = list(mark_dict_collection.find().sort("_id", 1).limit(1))[0]
+    else:
+        markdict = mark_dict_collection.find_one({"_id": ObjectId(oid)})
+    if markdict:
+        return markdict_detail_helper(markdict)
+    print("wrong oid")
+    return None
+    
+    
+    #### cache o
+    # cache_hit = markdictCache.find_by_oid(oid)
+    # if cache_hit:
+    #     print(" C A C H E   H I T !")
+    #     return cache_hit
 
-    print(f"{oid} is not in cache ")
-    # 캐시에 없을 경우 새로 추가.
-    search_option = form_search_option(m)
+    # print(f"{oid} is not in cache ")
+    # # 캐시에 없을 경우 새로 추가.
+    # search_option = form_search_option(m)
 
-    add_cache(m, search_option)
-    return markdictCache._cache[oid]
+    # add_cache(m, search_option)
+    # return markdictCache._cache[oid]
+    
     
 # previous
 def retrieve_previous(oid: str, m: MarkdictList):
-    curr_idx = markdictCache._cache_key_list.index(oid)
+    # if not oid:
+    #     print("not oid")
+    #     return None
+    # else:   
+    #     search_option = form_search_option(m)
+    #     search_option.append({"_id": {"$lt": ObjectId(oid)}})
+        
+    #     previous_data = list(
+    #         mark_dict_collection
+    #         .find({"$and": search_option})
+    #         .sort("_id", -1)
+    #         .limit(1)
+    #         )
     
-    # 특정페이지의 가장 첫번째 데이터일 때
-    if curr_idx % m.size == 0:
+    search_option = form_search_option(m)
+    search_option.append({"_id": {"$lt": ObjectId(oid)}})
+    
+    previous_data = list(
+        mark_dict_collection
+        .find({"$and": search_option})
+        .sort("_id", -1)
+        .limit(1)
+        )
+    
+    if previous_data:
+        previous_data = previous_data[0]
+        prev_oid = str(previous_data["_id"])
+        print(prev_oid, "==")
+        return prev_oid
+    else:
+        return None
+    
+    
+    # #### cache o
+    # curr_idx = markdictCache._cache_key_list.index(oid)
+    
+    # # 특정페이지의 가장 첫번째 데이터일 때
+    # if curr_idx % m.size == 0:
         
-        # 그 전값이 캐시에 있는지 확인
-        if curr_idx > 0:
-            prev_idx = curr_idx - 1
-            prev_oid = markdictCache._cache_key_list[prev_idx]
-            return prev_oid, True  
+    #     # 그 전값이 캐시에 있는지 확인
+    #     if curr_idx > 0:
+    #         prev_idx = curr_idx - 1
+    #         prev_oid = markdictCache._cache_key_list[prev_idx]
+    #         return prev_oid, True  
         
-        # 캐시에 없으면 -> DB
-        else:
-            if m.page <= 0:
-                return None, False
+    #     # 캐시에 없으면 -> DB
+    #     else:
+    #         if m.page <= 0:
+    #             return None, False
             
-            m.page =  m.page - 1
-            search_option = form_search_option(m)
-            add_prev_cache(m, search_option)
+    #         m.page =  m.page - 1
+    #         search_option = form_search_option(m)
+    #         add_prev_cache(m, search_option)
             
-            curr_idx = markdictCache._cache_key_list.index(oid)
-            prev_idx = curr_idx - 1
-            prev_oid = markdictCache._cache_key_list[prev_idx]
+    #         curr_idx = markdictCache._cache_key_list.index(oid)
+    #         prev_idx = curr_idx - 1
+    #         prev_oid = markdictCache._cache_key_list[prev_idx]
             
-            return prev_oid, True
+    #         return prev_oid, True
         
-    prev_idx = curr_idx - 1
-    prev_oid = markdictCache._cache_key_list[prev_idx]
-    return prev_oid, False
+    # prev_idx = curr_idx - 1
+    # prev_oid = markdictCache._cache_key_list[prev_idx]
+    # return prev_oid, False
+    
 
 
 # next
 def retrieve_next(oid: str, m: MarkdictList):
-    curr_idx = markdictCache._cache_key_list.index(oid)
+    # if not oid:
+    #     next_data = list(mark_dict_collection.find({"humanCheck": False}).sort("_id", 1).skip(1).limit(1))[0]
+    # else:
+    #     search_option = form_search_option(m)
+    #     search_option.append({"_id": {"$gt": ObjectId(oid)}})
+    #     next_data = mark_dict_collection.find_one({"$and": search_option})
     
-    if (curr_idx+1)%m.size == 0:
-        # 캐시에 있는지 확인
-        if curr_idx+1 < len(markdictCache._cache_key_list):
-            next_idx = curr_idx + 1
-            next_oid = markdictCache._cache_key_list[next_idx]
-            return next_oid, True
+    search_option = form_search_option(m)
+    search_option.append({"_id": {"$gt": ObjectId(oid)}})
+    next_data = mark_dict_collection.find_one({"$and": search_option})
+    
+    if next_data:
+        next_oid = str(next_data["_id"])
+        return next_oid
+    else:
+        return None
+    
+    # #### cache o
+    # curr_idx = markdictCache._cache_key_list.index(oid)
+    
+    # if (curr_idx+1)%m.size == 0:
+    #     # 캐시에 있는지 확인
+    #     if curr_idx+1 < len(markdictCache._cache_key_list):
+    #         next_idx = curr_idx + 1
+    #         next_oid = markdictCache._cache_key_list[next_idx]
+    #         return next_oid, True
         
-        # 캐시에 없으면 -> DB
-        else:
-            m.page = m.page + 1
-            search_option = form_search_option(m)
-            add_cache(m, search_option)
+    #     # 캐시에 없으면 -> DB
+    #     else:
+    #         m.page = m.page + 1
+    #         search_option = form_search_option(m)
+    #         add_cache(m, search_option)
             
-            next_idx = curr_idx + 1
-            next_oid = markdictCache._cache_key_list[next_idx]
-            return next_oid, True
+    #         next_idx = curr_idx + 1
+    #         next_oid = markdictCache._cache_key_list[next_idx]
+    #         return next_oid, True
 
-    if curr_idx+1 == len(markdictCache._cache_key_list):
-        return None, False
+    # if curr_idx+1 == len(markdictCache._cache_key_list):
+    #     return None, False
     
-    next_idx = curr_idx + 1
-    next_oid = markdictCache._cache_key_list[next_idx]
-    return next_oid, False
+    # next_idx = curr_idx + 1
+    # next_oid = markdictCache._cache_key_list[next_idx]
+    # return next_oid, False
+
 
 # Update
 def update_db(oid: str, user_input_list: list, result_status: str, worker: str):
@@ -147,16 +215,16 @@ def update_db(oid: str, user_input_list: list, result_status: str, worker: str):
     )
 
 
-def update_cache(oid: str, user_input_list: list, input_filter: str, worker: str):
-    markdictCache._cache[oid]["humanCheck"] = True
-    markdictCache._cache[oid]["passCheck"] = False
-    markdictCache._cache[oid]["directInput"] = user_input_list
-    markdictCache._cache[oid]["resultStatus"] = input_filter
-    markdictCache._cache[oid]["worker"] = worker
+# def update_cache(oid: str, user_input_list: list, input_filter: str, worker: str):
+#     markdictCache._cache[oid]["humanCheck"] = True
+#     markdictCache._cache[oid]["passCheck"] = False
+#     markdictCache._cache[oid]["directInput"] = user_input_list
+#     markdictCache._cache[oid]["resultStatus"] = input_filter
+#     markdictCache._cache[oid]["worker"] = worker
 
 
 # 보류(pass)
 def add_pass_list(oid: str):
-    markdictCache._cache[oid]["passCheck"] = True
+    # markdictCache._cache[oid]["passCheck"] = True
     oid = ObjectId(oid)
     mark_dict_collection.update_one({"_id": oid}, {"$set": {"Check.passCheck": True}})
